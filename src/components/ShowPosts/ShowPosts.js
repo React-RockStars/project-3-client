7e01f3f9093b8c35323940efeda747b3f51f8cdf
laurenmenendez <menendez98@gmail.com>
2021-05-13 13:53:33 -0400

    Add accordion to comment display

--- src/components/ShowPosts/ShowPosts.js
+++ src/components/ShowPosts/ShowPosts.js
@@ -4,6 +4,7 @@ import messages from '../AutoDismissAlert/messages'
 import { showPosts, deletePost } from '../../api/post'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Accordion from 'react-bootstrap/Accordion'
 
 class ShowPosts extends Component {
   constructor () {
@@ -66,42 +67,49 @@ class ShowPosts extends Component {
         <div>
           {this.state.posts.map(post => (
             <div key={post._id}>
-              <Card className="text-left">
-                <Card.Header>Post</Card.Header>
-                <Card.Body>
-                  <Card.Title>{post.title}</Card.Title>
-                  <Card.Text>
-                    {post.body}
-                  </Card.Text>
-                  {this.props.user._id === post.owner ? <Button href={`#/posts/${post._id}/edit-post`}>
-                  Edit</Button> : ''}
-                  {this.props.user._id === post.owner ? <Button href={`#/posts/${post._id}/delete-post`}>
-                  Delete</Button> : ''}
-                  <Button href={`#/create-comment/${post._id}`}>
-                  Add comment</Button>
-                </Card.Body>
-                <Card.Footer className="text-muted">{post.updatedAt}</Card.Footer>
-              </Card>
-              {post.comments.map(comment => (
-                <div key={comment._id}>
-                  <Card className="text-left">
-                    <Card.Header>{comment.owner}</Card.Header>
-                    <Card.Body>
-                      <Card.Text>
-                        {comment.content}
-                      </Card.Text>
-                      <Button href={`#/comments/${comment._id}/edit-comment/${post._id}`}>
-                      Edit</Button>
-                      <Button href={`#/comments/${comment._id}/delete-comment/${post._id}`}>
-                      Delete</Button>
-                    </Card.Body>
-                    <Card.Footer className="text-muted">{comment.timestamp}</Card.Footer>
-                  </Card>
-                </div>
-              ))
-              }
+              <div className="mx-auto mt-5">
+                <Card className="text-left">
+                  <Card.Header>Post</Card.Header>
+                  <Card.Body>
+                    <Card.Title>{post.title}</Card.Title>
+                    <Card.Text>
+                      {post.body}
+                    </Card.Text>
+                    {this.props.user._id === post.owner ? <Button href={`#/posts/${post._id}/edit-post`}>
+                    Edit</Button> : ''}
+                    {this.props.user._id === post.owner ? <Button href={`#/posts/${post._id}/delete-post`}>
+                    Delete</Button> : ''}
+                    <Button href={`#/create-comment/${post._id}`}>
+                    Add comment</Button>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">{post.updatedAt}</Card.Footer>
+                </Card>
+                {post.comments.length > 0
+                  ? <Accordion>
+                    <Card>
+                      <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                          View comments
+                        </Accordion.Toggle>
+                      </Card.Header>
+                      <Accordion.Collapse eventKey="0">
+                        <Card.Body>{post.comments.map(comment => (
+                          <div key={comment._id}>
+                            <p>{comment.content}</p>
+                            <Button href={`#/comments/${comment._id}/edit-comment/${post._id}`}>
+                          Edit</Button>
+                            <Button href={`#/comments/${comment._id}/delete-comment/${post._id}`}>
+                          Delete</Button>
+                          </div>
+                        ))}
+                        </Card.Body>
+                      </Accordion.Collapse>
+                    </Card>
+                  </Accordion> : ''}
+              </div>
             </div>
-          ))}
+          ))
+          }
         </div>
       )
     }
